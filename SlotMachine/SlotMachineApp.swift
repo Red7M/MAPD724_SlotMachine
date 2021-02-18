7a822217d7c26ee6b06e7b95ac9189e71cbf75b4
Red7M <reda.mehali@gmail.com>
2021-02-18 16:06:08 -0500

    Initial Commit

---
+++ SlotMachine/SlotMachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotMachineApp.swift
+//  SlotMachine
+//
+//  Created by Reda Mehali on 2/18/21.
+//
+
+import SwiftUI
+
+@main
+struct SlotMachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
